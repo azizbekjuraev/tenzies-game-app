fbaf9a982323f7bf3b8710263551220c9bdf428a
AZIZBEKJURAEV <ajuraev98@mail.ru>
2023-01-07 01:21:14 +0500

    holdDice func added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
 import React from "react";
 import Die from "./components/Die";
+import { nanoid } from "nanoid";
 
 function App() {
   function allNewDice() {
     const newDice = [];
     for (let i = 0; i < 10; i++) {
-      newDice.push(Math.floor(Math.random() * 6) + 1);
+      newDice.push({
+        value: Math.floor(Math.random() * 6),
+        isHeld: false,
+        id: nanoid(),
+      });
     }
     return newDice;
   }
+
   const [dice, setDice] = React.useState(allNewDice());
-  console.log(allNewDice());
+
+  function rollDice() {
+    setDice(allNewDice());
+  }
+
+  function holdDice(id) {
+    setDice((oldDice) =>
+      oldDice.map((die) => {
+        return die.id === id ? { ...die, isHeld: !die.isHeld } : die;
+      })
+    );
+  }
 
   const diceM = dice.map((item) => {
-    return <Die value={item} />; //nomerni olish uchun value ishlatamiza!!
+    return (
+      <Die
+        value={item.value}
+        isHeld={item.isHeld}
+        key={item.id}
+        holdDice={() => holdDice(item.id)}
+      />
+    ); //nomerni olish uchun ishlatamiza!!
   });
-
   return (
     <main className="main">
       <div className="dice-container">{diceM}</div>
-      <button onClick={() => setDice(allNewDice())}>Roll</button>
+      <button onClick={rollDice}>Roll</button>
     </main>
   );
 }
--- src/components/Die.jsx
+++ src/components/Die.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 function Die(props) {
+  const styles = {
+    backgroundColor: props.isHeld ? "green" : "",
+  };
+
   return (
-    <div className="die-face">
+    <div className="die-face" style={styles} onClick={props.holdDice}>
       <h2 className="die-num">{props.value}</h2>
     </div>
   );
